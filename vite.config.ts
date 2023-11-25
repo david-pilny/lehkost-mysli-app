28c8f39fac1df5fabbd13436819bc3db5a75555c
david-pilny <93579404+david-pilny@users.noreply.github.com>
2023-11-25 23:30:08 +0100

    feat(site-structure): route aliases, skeleton etc.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@media': path.resolve(__dirname, 'src/media'),
+      '@views': path.resolve(__dirname, 'src/views'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
